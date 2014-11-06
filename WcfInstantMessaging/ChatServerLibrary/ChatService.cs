729384878feba106f0fec28cd0353a0bf21f7c28
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-06 15:45:06 +0000

    Adding Chat Service

--- WcfInstantMessaging/ChatServerLibrary/ChatService.cs
+++ WcfInstantMessaging/ChatServerLibrary/ChatService.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
+﻿using System.Diagnostics;
 
 namespace ChatServerLibrary
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChatService" in both code and config file together.
     public class ChatService : IChatService
     {
-        public void DoWork()
+        public void PostNote(string from, string note)
         {
+            Debug.WriteLine("{0}: {1}", from, note);
         }
     }
 }
--- WcfInstantMessaging/ChatServerLibrary/IChatService.cs
+++ WcfInstantMessaging/ChatServerLibrary/IChatService.cs
@@ -7,11 +7,10 @@ using System.Text;
 
 namespace ChatServerLibrary
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IChatService" in both code and config file together.
     [ServiceContract]
     public interface IChatService
     {
         [OperationContract]
-        void DoWork();
+        void PostNote(string from, string note);
     }
 }
