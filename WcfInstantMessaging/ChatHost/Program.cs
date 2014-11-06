c723d150b9e7b6dc10e0108cd88871806f1e61df
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-06 16:37:54 +0000

    Add WCF Host and Client

---
+++ WcfInstantMessaging/ChatHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceModel;
+using ChatServerLibrary;
+
+namespace ChatHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof (ChatService)))
+            {
+                host.Open();
+
+                Console.WriteLine("Service Ready");
+                Console.ReadKey();
+            }
+        }
+    }
+}
