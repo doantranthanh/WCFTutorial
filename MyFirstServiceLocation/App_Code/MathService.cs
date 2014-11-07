8d710fd2384a6574e081d2bfe30a343820a0cd70
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-07 11:50:14 +0000

    Deploy Service Client

---
+++ MyFirstServiceLocation/App_Code/MathService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IMathService
+{
+    public int Addition(Math obj1)
+    {
+        return (obj1.Number1 + obj1.Number2);
+    }
+
+    public int Subtraction(Math obj2)
+    {
+        try
+        {
+            return (obj2.Number1 - obj2.Number2);
+        }
+        catch (Exception)
+        {
+            return 0;
+        }
+    }
+
+    public int Mutiplication(Math obj3)
+    {
+        return obj3.Number1*obj3.Number2;
+    }
+
+    public int Division(Math obj4)
+    {
+        if (obj4.Number2 != 0)
+        {
+            return obj4.Number1/obj4.Number2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
