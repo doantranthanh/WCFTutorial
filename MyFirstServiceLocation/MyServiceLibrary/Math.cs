50d1190d1e434e0fc0ebabd8e5374b1721efbdc8
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-07 11:15:01 +0000

    Adding DataMember to Math Class

--- MyFirstServiceLocation/MyServiceLibrary/Math.cs
+++ MyFirstServiceLocation/MyServiceLibrary/Math.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace MyServiceLibrary
 {
-    class Math
+    [DataContract]
+    public class Math
     {
+        [DataMember]
+        public int Number1 { get; set; }
+
+        [DataMember]
+        public int Number2 { get; set; }
     }
 }
