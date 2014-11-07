1ce4ae5516c4a6e6e4e473de9a890bde8de1608f
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-07 11:18:56 +0000

    Define Operation Contract for Math Service

--- MyFirstServiceLocation/MyServiceLibrary/IMaths.cs
+++ MyFirstServiceLocation/MyServiceLibrary/IMaths.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 
 namespace MyServiceLibrary
 {
+    [ServiceContract]
     public interface IMaths
     {
+        [OperationContract]
+        int Addition(Math obj1);
+
+        [OperationContract]
+        int Subtraction(Math obj2);
+
+        [OperationContract]
+        int Mutiplication(Math obj3);
+
+        [OperationContract]
+        int Division(Math obj4);
     }
 }
