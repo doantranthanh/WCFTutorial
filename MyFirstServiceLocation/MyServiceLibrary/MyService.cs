f73927548e8f249e29c6d7d65d7c9841a5bcbfee
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-07 11:21:59 +0000

    Implementing Math Service

--- MyFirstServiceLocation/MyServiceLibrary/MyService.cs
+++ MyFirstServiceLocation/MyServiceLibrary/MyService.cs
@@ -5,7 +5,40 @@ using System.Text;
 
 namespace MyServiceLibrary
 {
-    public class MyService
+    public class MyService: IMaths
     {
+        public int Addition(Math obj1)
+        {
+            return (obj1.Number1 + obj1.Number2);
+        }
+
+        public int Subtraction(Math obj2)
+        {
+            try
+            {
+                return (obj2.Number1 - obj2.Number2);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public int Mutiplication(Math obj3)
+        {
+            return obj3.Number1*obj3.Number2;
+        }
+
+        public int Division(Math obj4)
+        {
+            if (obj4.Number2 != 0)
+            {
+                return obj4.Number1/obj4.Number2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
