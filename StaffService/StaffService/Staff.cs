23eaa160311e2950f86b3706e9ed082ff075a3db
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-09 10:49:19 +0000

    Adding Staff class

---
+++ StaffService/StaffService/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaffService
+{
+    public class Staff
+    {
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
