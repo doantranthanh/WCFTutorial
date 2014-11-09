ee6cf2b19b403545386f437117ea8545f766cece
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-09 12:22:25 +0000

    Adding Service Host and Service Client

---
+++ StaffService/StaffClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using StaffClient.ServiceReference1;
+
+namespace StaffClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StaffServiceClient sh = new StaffServiceClient())
+            {
+                sh.Open();
+                sh.CreateListStaff();
+                Console.WriteLine(sh.DisplayStaff());
+                Console.WriteLine(sh.GetBirthday(1).ToShortDateString());
+                Console.WriteLine(sh.GetBirthday(2).ToShortDateString());
+                Console.WriteLine(sh.GetBirthday(3).ToShortDateString());
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ StaffService/StaffServiceHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceModel;
+using StaffService;
+
+namespace StaffServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost sh = new ServiceHost(typeof(Staff)))
+            {
+                sh.Open();
+                Console.WriteLine("Staff Service opened successfully");
+                Console.WriteLine("Press Enter to terminate Staff Service");
+                Console.ReadLine();
+            }
+        }
+    }
+}
